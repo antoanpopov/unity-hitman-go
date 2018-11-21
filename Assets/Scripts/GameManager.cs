3e04dba79a1cccfa4be43b9a22f0a0e6c1b62fd7
Antoan Popov <antoan.popov@mentormate.com>
2018-11-21 18:07:20 +0200

    Added GraphicMover and animations to end and start screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,93 @@
 ﻿using System;
-using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.Events; using UnityEngine.SceneManagement;  public class GameManager : MonoBehaviour {      Board _board;     PlayerManager _player;      bool _hasLevelStarted = false;     public bool HasLevelStarted { get { return _hasLevelStarted; } set { _hasLevelStarted = value; } }      bool _isGamePlaying = false;     public bool IsGamePlaying { get { return _isGamePlaying; } set { _isGamePlaying = value; } }      bool _isGameOver = false;     public bool IsGameOver { get { return _isGameOver; } set { _isGameOver = value; } }      bool _hasLevelFinished = false;     public bool HasLevelFinished { get { return _hasLevelFinished; } set { _hasLevelFinished = value; } }      public float delay = 1f;      public UnityEvent startLevelEvent;     public UnityEvent playLevelEvent;     public UnityEvent endLevelEvent;      // Use this for initialization     void Awake() {         _board = FindObjectOfType<Board>().GetComponent<Board>();         _player = FindObjectOfType<PlayerManager>().GetComponent<PlayerManager>();     }      private void Start() {         if (_player != null && _board != null) {             StartCoroutine(RunGameLoop());         } else {             Debug.LogWarning("GAMEMANAGER ERROR: no player or board found");         }     }      IEnumerator RunGameLoop() {         yield return StartCoroutine(StartLevelCoroutine());         yield return StartCoroutine(PlayLevelCoroutine());         yield return StartCoroutine(EndLevelCoroutine());     }      IEnumerator StartLevelCoroutine() {          Debug.Log("START LEVEL");          _player.playerInput.inputEnabled = false;         while (!_hasLevelStarted) {             //Show start screen             yield return null;         }          if(startLevelEvent != null) {             startLevelEvent.Invoke();         }     }     IEnumerator PlayLevelCoroutine() {          Debug.Log("PLAY LEVEL");          _isGamePlaying = true;         yield return new WaitForSeconds(delay);         _player.playerInput.inputEnabled = true;          if (playLevelEvent != null) {             playLevelEvent.Invoke();         }          while (!_isGameOver) {             yield return null;              _isGameOver = IsWinner();          }          Debug.Log("WIN! ==========");     }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    Board _board;
+    PlayerManager _player;
+
+    bool _hasLevelStarted = false;
+    public bool HasLevelStarted { get { return _hasLevelStarted; } set { _hasLevelStarted = value; } }
+
+    bool _isGamePlaying = false;
+    public bool IsGamePlaying { get { return _isGamePlaying; } set { _isGamePlaying = value; } }
+
+    bool _isGameOver = false;
+    public bool IsGameOver { get { return _isGameOver; } set { _isGameOver = value; } }
+
+    bool _hasLevelFinished = false;
+    public bool HasLevelFinished { get { return _hasLevelFinished; } set { _hasLevelFinished = value; } }
+
+    public float delay = 1f;
+
+    public UnityEvent setupEvent;
+    public UnityEvent startLevelEvent;
+    public UnityEvent playLevelEvent;
+    public UnityEvent endLevelEvent;
+
+    // Use this for initialization
+    void Awake() {
+        _board = FindObjectOfType<Board>().GetComponent<Board>();
+        _player = FindObjectOfType<PlayerManager>().GetComponent<PlayerManager>();
+    }
+
+    private void Start() {
+        if (_player != null && _board != null) {
+            StartCoroutine(RunGameLoop());
+        } else {
+            Debug.LogWarning("GAMEMANAGER ERROR: no player or board found");
+        }
+    }
+
+    IEnumerator RunGameLoop() {
+        yield return StartCoroutine(StartLevelCoroutine());
+        yield return StartCoroutine(PlayLevelCoroutine());
+        yield return StartCoroutine(EndLevelCoroutine());
+    }
+
+    IEnumerator StartLevelCoroutine() {
+
+        Debug.Log("SETUP LEVEL");
+        if(setupEvent != null) {
+            setupEvent.Invoke();
+        }
+        Debug.Log("START LEVEL");
+
+        _player.playerInput.inputEnabled = false;
+        while (!_hasLevelStarted) {
+            //Show start screen
+            yield return null;
+        }
+
+        if(startLevelEvent != null) {
+            startLevelEvent.Invoke();
+        }
+    }
+    IEnumerator PlayLevelCoroutine() {
+
+        Debug.Log("PLAY LEVEL");
+
+        _isGamePlaying = true;
+        yield return new WaitForSeconds(delay);
+        _player.playerInput.inputEnabled = true;
+
+        if (playLevelEvent != null) {
+            playLevelEvent.Invoke();
+        }
+
+        while (!_isGameOver) {
+            yield return null;
+
+            _isGameOver = IsWinner();
+
+        }
+
+        Debug.Log("WIN! ==========");
+    }
 
     private bool IsWinner() {
         if(_board.PlayerNode == _board.GoalNode) {
@@ -8,4 +96,29 @@ using System.Collections; using System.Collections.Generic; using UnityEngin
         return false;
     }
 
-    IEnumerator EndLevelCoroutine() {         Debug.Log("END LEVEL");         _player.playerInput.inputEnabled = false;          if (endLevelEvent != null) {             endLevelEvent.Invoke();         }          while (!_hasLevelFinished) {               yield return null;         }          RestartLevel();     }      void RestartLevel() {         Scene scene = SceneManager.GetActiveScene();         SceneManager.LoadScene(scene.name);     }      public void PlayLevel() {         _hasLevelStarted = true;     } } 
+    IEnumerator EndLevelCoroutine() {
+        Debug.Log("END LEVEL");
+        _player.playerInput.inputEnabled = false;
+
+        if (endLevelEvent != null) {
+            endLevelEvent.Invoke();
+        }
+
+        while (!_hasLevelFinished) {
+
+
+            yield return null;
+        }
+
+        RestartLevel();
+    }
+
+    void RestartLevel() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void PlayLevel() {
+        _hasLevelStarted = true;
+    }
+}
---
+++ Assets/Scripts/GraphicMover.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public enum GraphicMoverMode {
+    MoveTo,
+    ScaleTo,
+    MoveFrom
+}
+
+public class GraphicMover : MonoBehaviour {
+
+    public GraphicMoverMode mode;
+
+    public Transform startXform;
+
+    public Transform endXform;
+
+    public float moveTime = 1f;
+    public float delay = 0f;
+    public LoopType loopType = LoopType.Restart;
+    public int loopTimes = 1;
+    public Ease easeType = Ease.OutExpo;
+
+
+    private void Awake() {
+        if (endXform == null) {
+            endXform = new GameObject(gameObject.name + "XformEnd").transform;
+            endXform.position = transform.position;
+            endXform.rotation = transform.rotation;
+            endXform.localScale = transform.localScale;
+        }
+
+        if (startXform == null) {
+            startXform = new GameObject(gameObject.name + "XformStart").transform;
+            startXform.position = transform.position;
+            startXform.rotation = transform.rotation;
+            startXform.localScale = transform.localScale;
+        }
+    }
+
+    public void Reset() {
+        switch (mode) {
+            case GraphicMoverMode.MoveTo:
+                if (startXform != null) {
+                    transform.position = startXform.position;
+                }
+                break;
+            case GraphicMoverMode.MoveFrom:
+                if (endXform != null) {
+                    transform.position = endXform.position;
+                }
+                break;
+            case GraphicMoverMode.ScaleTo:
+                if(startXform != null) {
+                    transform.localScale = startXform.localScale;
+                }              
+                break;
+        }
+    }
+
+    public void Move() {
+        switch (mode) {
+            case GraphicMoverMode.MoveTo:
+                transform.DOMove(endXform.position, moveTime).SetEase(easeType).SetDelay(delay).SetLoops(loopTimes, loopType);
+                break;
+            case GraphicMoverMode.MoveFrom:
+                transform.DOMove(startXform.position, moveTime).SetEase(easeType).SetDelay(delay).SetLoops(loopTimes, loopType);
+                break;
+            case GraphicMoverMode.ScaleTo:
+                transform.DOScale(endXform.localScale, moveTime).SetEase(easeType).SetDelay(delay).SetLoops(loopTimes, loopType);
+                break;
+        }
+    }
+}
